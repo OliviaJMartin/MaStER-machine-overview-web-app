6315d0f364918929155806cd41dd22dfa7f35667
OliviaJMartin <Medina-Ohio2>
2021-08-10 10:56:08 +0100

    css changes to the three input boxes to make them uniform

--- JavaScript/connections.js
+++ JavaScript/connections.js
@@ -6,21 +6,21 @@ function outputMachine() {
 
 //function to move from the users current page to the overview page
 function overview() {
-    location.href = "http://localhost:63342/MaStER-machine-overview-web-app1/templates/Overview.html?_ijt=th9rde00p82sa3m7n48uo4l42d";
+    location.href = "http://localhost:63342/MaStER-machine-overview-web-app-3/templates/Overview.html?_ijt=a8dvk01a6142d6kn54lc4rsbtr&_ij_reload";
 }
 
 //function to move from the users current page to the login page
 function login(machineName) {
-    location.href = "http://localhost:63342/MaStER-machine-overview-web-app1/templates/Login.html?_ijt=th9rde00p82sa3m7n48uo4l42d";
+    location.href = "http://localhost:63342/MaStER-machine-overview-web-app-3/templates/Login.html?_ijt=a8dvk01a6142d6kn54lc4rsbtr&_ij_reload";
     sessionStorage.setItem("machineName", machineName);
 }
 
 //function to move from the users current page to the machine status page
 function machineStatus(){
-    location.href = "http://localhost:63342/MaStER-machine-overview-web-app1/templates/MachineStatus.html?_ijt=th9rde00p82sa3m7n48uo4l42d";
+    location.href = "http://localhost:63342/MaStER-machine-overview-web-app-3/templates/MachineStatus.html?_ijt=a8dvk01a6142d6kn54lc4rsbtr&_ij_reload";
 }
 
 //function to move from the users current page to the update status page
 function updateStatus(){
-    location.href = "http://localhost:63342/MaStER-machine-overview-web-app1/templates/UpdateStatus.html?_ijt=th9rde00p82sa3m7n48uo4l42d";
+    location.href = "http://localhost:63342/MaStER-machine-overview-web-app-3/templates/UpdateStatus.html?_ijt=a8dvk01a6142d6kn54lc4rsbtr&_ij_reload";
 }
