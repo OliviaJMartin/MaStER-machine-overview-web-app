e0efa84458c7be87e603bbbf140b868935e92a98
OliviaJMartin <Medina-Ohio2>
2021-09-08 11:05:51 +0100

    variable handling with constants and circles change colour

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -6,7 +6,6 @@ from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-
 class overview(View):
     template = 'Overview.html'
 
@@ -17,24 +16,23 @@ class overview(View):
         #     # request.session['LA1_status'] = i.Status
         #     print(i.Status)
         return render(request, self.template,
-                      context=
-                          {
-                              'LA1_status': 'On Service',
-                              'LA2_status': 'Fully Serviceable',
-                              'LA4_status': 'Clinical With Limitations',
-                              'CT_status': 'Unserviceable',
-                              'Brachy_status': 'Fully Serviceable',
-                              'LA1_user': 'Testing',
-                              'LA2_user': 'Checking',
-                              'LA4_user': 'Checking',
-                              'CT_user': 'Testing',
-                              'Brachy_user': 'OlMartin',
-                              'LA1_time': '08:00',
-                              'LA2_time': '09:00',
-                              'LA4_time': '10:00',
-                              'CT_time': '11:00',
-                              'Brachy_time': '12:00'
-                          },
+                      context={
+                          'LA1_status': 'On Service / PMI',
+                          'LA2_status': 'Fully Serviceable',
+                          'LA4_status': 'Clinical With Limitations',
+                          'CT_status': 'Unserviceable',
+                          'Brachy_status': 'Fully Serviceable',
+                          'LA1_user': 'Testing',
+                          'LA2_user': 'Checking',
+                          'LA4_user': 'Checking',
+                          'CT_user': 'Testing',
+                          'Brachy_user': 'OlMartin',
+                          'LA1_time': '08:00',
+                          'LA2_time': '09:00',
+                          'LA4_time': '10:00',
+                          'CT_time': '11:00',
+                          'Brachy_time': '12:00',
+                      },
                       )
 
 
@@ -43,7 +41,10 @@ class loggingin(View):
 
     def get(self, request):
         form = AuthenticationForm()
-        return render(request, self.template, {'form': form})
+        return render(request, self.template, context={
+            'form': form,
+            'Machine_Name': 'LA4',
+        }, )
 
     def post(self, request):
         form = AuthenticationForm(request.POST)
@@ -54,7 +55,10 @@ class loggingin(View):
             login(request, user)
             return HttpResponseRedirect('/machinestatus/')
         else:
-            return render(request, self.template, {'form': form})
+            return render(request, self.template, context={
+                'form': form,
+                'Machine_Name': 'LA4',
+            }, )
 
 
 class status(LoginRequiredMixin, View):
@@ -62,7 +66,14 @@ class status(LoginRequiredMixin, View):
     login_url = '/login/'
 
     def get(self, request):
-        return render(request, self.template)
+        return render(request, self.template, context={
+                'Machine_Name': 'LA4',
+                'Serial_No': '2459',
+                'SW_Version': 'TB v2.5',
+                'Machine_Status': 'Clinical With Limitations',
+                'Current_Owner': 'Olivia Martin',
+                'Start_Time': ''
+            },)
 
 
 class loggingout(View):
@@ -77,4 +88,11 @@ class update(View):
     template = 'UpdateStatus.html'
 
     def get(self, request):
-        return render(request, self.template)
+        return render(request, self.template, context={
+                'Machine_Name': 'LA4',
+                'User': 'OlMartin',
+            },)
+
+
+class recentStatus(View):
+    template = 'x'
